4fbb8d9d873ccb49194c8444d3eca601ad3844e4
Tom Stewart <thomas@ascent.ai>
2018-08-17 11:47:54 +0900

    upping omega

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     logger.configure(dir=log_dir, format_strs=['tensorboard', 'stdout'])
 
     env = ProstheticsEnv(visualize=False)
-    wrapped_env = ReferenceMotionWrapper(env, motion_file='mocap_data/running_guy_loop.bvh', rsi=False, omega=50.)
+    wrapped_env = ReferenceMotionWrapper(env, motion_file='mocap_data/running_guy_loop.bvh', rsi=False, omega=100.)
 
     with tf.Session() as sess:
         pi = MlpPolicy(name='pi', action_shape=wrapped_env.action_space.shape, observation_shape=wrapped_env.observation_space.shape, hid_size=64, num_hid_layers=3)
